d9cf230f3cbc9a698da068ad0e85410e866fd9e1
452Harsh <103653899+452Harsh@users.noreply.github.com>
2023-10-02 12:46:39 +0530

    2

---
+++ components/ChatBot.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ChatBot = () => {
+    return (
+        <div className='text-5xl'>ChatBot</div>
+    )
+}
+
+export default ChatBot
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import ChatBot from '../../components/ChatBot'
 
 export default function Home() {
   return (
     <div>
       <div className='text-5xl'>PrakritiChatbot</div>
+      <ChatBot />
     </div>
 
   )
