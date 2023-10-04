28c78037c519c07b0796fcaa4cb0103ce6e57501
452Harsh <103653899+452Harsh@users.noreply.github.com>
2023-10-04 21:44:30 +0530

    3

---
+++ components/HomePage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import HeroSection from './HeroSection'
+import Section from './Section'
+import Footer from './Footer'
+
+const HomePage = () => {
+    return (
+        <div>
+            <Header />
+            <HeroSection />
+            <Section />
+            <Footer />
+        </div>
+    )
+}
+
+export default HomePage
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from 'next/image'
-import ChatBot from '../../components/ChatBot'
+import HomePage from '../../components/HomePage'
 
 export default function Home() {
   return (
-    <div>
-      <div className='text-5xl'>PrakritiChatbot</div>
-      <ChatBot />
+    <div >
+      <HomePage />
+
     </div>
 
   )
